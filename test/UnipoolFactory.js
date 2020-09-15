ef0d1af179463f3f4aa70d5a80f05f7119c3cafb
Oliver <onbjerg@users.noreply.github.com>
2020-09-16 01:18:17 +0200

    Add UnipoolFactory (#2)

---
+++ test/UnipoolFactory.js
@@ -0,0 +1,25 @@
+const { BN, expectRevert } = require('openzeppelin-test-helpers');
+const { expect } = require('chai');
+
+const UniswapToken = artifacts.require('UniswapTokenMock');
+const TradedToken = artifacts.require('HoneyTokenMock');
+const UnipoolFactory = artifacts.require('UnipoolFactory');
+
+contract('UnipoolFactory', function ([_, wallet1, wallet2, wallet3, wallet4]) {
+    describe('UnipoolFactory', async function () {
+        beforeEach(async function () {
+            this.uniswapToken = await UniswapToken.new();
+            this.tradedToken = await TradedToken.new(wallet1);
+            this.factory = await UnipoolFactory.new();
+        });
+
+        it('creates a new Unipool for a given LP token', async function () {
+            await this.factory.createUnipool(this.uniswapToken.address);
+        });
+
+        it('does not allow duplicate Unipools', async function () {
+            await this.factory.createUnipool(this.uniswapToken.address);
+            await expectRevert(this.factory.createUnipool(this.uniswapToken.address), 'Pool already exists.');
+        });
+    });
+});
