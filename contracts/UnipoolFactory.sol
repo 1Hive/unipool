6aab4c1b88a0f7d0ab135bd5a7bf1ff87f512ed4
Felipe Novaes F Rocha <windholyghost@gmail.com>
2023-01-16 16:25:00 -0300

    feat: goerli network with env file

--- contracts/UnipoolFactory.sol
+++ contracts/UnipoolFactory.sol
@@ -6,9 +6,9 @@ import "./UnipoolRewardDepositor.sol";
 
 // Rinkeby deployment: 0x710D70591C70B1aA96d558269193F0e59F52E154
 // xDai deployment: 0xD38EB36B7E8b126Ff1E9fDD007bC4050B6C6aB7c
+// Goerli deployment: 0x67e278df46fce316aae1c67007d89c65c4257b7b
 
 contract UnipoolFactory {
-
     event NewUnipool(Unipool unipool);
     event NewRewardDepositor(UnipoolRewardDepositor unipoolRewardDepositor);
 
@@ -19,9 +19,15 @@ contract UnipoolFactory {
         return unipool;
     }
 
-    function newUnipoolWithDepositor(IERC20 _rewardToken) public returns (Unipool, UnipoolRewardDepositor) {
+    function newUnipoolWithDepositor(IERC20 _rewardToken)
+        public
+        returns (Unipool, UnipoolRewardDepositor)
+    {
         Unipool unipool = newUnipool(_rewardToken);
-        UnipoolRewardDepositor unipoolRewardDepositor = new UnipoolRewardDepositor(unipool, _rewardToken);
+        UnipoolRewardDepositor unipoolRewardDepositor = new UnipoolRewardDepositor(
+                unipool,
+                _rewardToken
+            );
         emit NewRewardDepositor(unipoolRewardDepositor);
 
         return (unipool, unipoolRewardDepositor);
--- truffle-config.js
+++ truffle-config.js
@@ -1,27 +1,43 @@
-const TruffleConfig = require('@aragon/truffle-config-v5/truffle-config')
-const HDWalletProvider = require('@truffle/hdwallet-provider')
+const TruffleConfig = require("@aragon/truffle-config-v5/truffle-config");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 // See <http://truffleframework.com/docs/advanced/configuration>
 // to customize your Truffle configuration!
 
-TruffleConfig.networks.development = {
-    host: 'localhost',
-    port: 9545,
-    network_id: '*',
-    gas: 8000000,
-    gasPrice: 1000000000, // web3.eth.gasPrice
-};
+// TruffleConfig.networks.development = {
+//   host: "localhost",
+//   port: 9545,
+//   network_id: "*",
+//   gas: 8000000,
+//   gasPrice: 1000000000, // web3.eth.gasPrice
+// };
+
+require("dotenv").config();
 
 TruffleConfig.networks.xdai = {
-   provider: function() {
-       return new HDWalletProvider(
-           process.env.MNEMONIC,
-           "https://dai.poa.network"
-       )
-   },
-   network_id: 100,
-   gas: 2000000
-}
+  provider: function () {
+    return new HDWalletProvider(
+      process.env.MNEMONIC,
+      "https://dai.poa.network"
+    );
+  },
+  network_id: 100,
+  gas: 2000000,
+};
+
+TruffleConfig.networks.goerli = {
+  provider: function () {
+    return new HDWalletProvider(
+      process.env.MNEMONIC,
+      `https://goerli.infura.io/v3/${process.env.INFURA_GOERLI}`
+    );
+  },
+  network_id: 5,
+  gas: 2000000,
+};
+
+TruffleConfig.networks.goerli.skipDryRun = true;
+TruffleConfig.networks.goerli.gasPrice = 1e11; // 100 Gwei
 
 TruffleConfig.networks.rinkeby.skipDryRun = true;
 TruffleConfig.networks.rinkeby.gasPrice = 1e11; // 100 Gwei
@@ -30,33 +46,33 @@ TruffleConfig.networks.rinkeby.gasPrice = 1e11; // 100 Gwei
 TruffleConfig.networks.mainnet.gasPrice = 1e11; // 100 Gwei
 
 TruffleConfig.compilers = {
-    solc: {
-        version: '0.5.12',
-        settings: {
-            optimizer: {
-                enabled: true,
-                runs: 200,
-            }
-        }
-    }
+  solc: {
+    version: "0.5.12",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
 
-TruffleConfig.mocha = { // https://github.com/cgewecke/eth-gas-reporter
-    reporter: 'eth-gas-reporter',
-    reporterOptions : {
-        currency: 'USD',
-        gasPrice: 10,
-        onlyCalledMethods: true,
-        showTimeSpent: true,
-        excludeContracts: ['Migrations']
-    }
+TruffleConfig.mocha = {
+  // https://github.com/cgewecke/eth-gas-reporter
+  reporter: "eth-gas-reporter",
+  reporterOptions: {
+    currency: "USD",
+    gasPrice: 10,
+    onlyCalledMethods: true,
+    showTimeSpent: true,
+    excludeContracts: ["Migrations"],
+  },
 };
 
-TruffleConfig.plugins = ['truffle-plugin-verify'];
+TruffleConfig.plugins = ["truffle-plugin-verify"];
 
 TruffleConfig.api_keys = {
-    etherscan: "Etherscan API Key"
+  etherscan: process.env.ETHERSCAN_API_KEY,
 };
 
 module.exports = TruffleConfig;
-
